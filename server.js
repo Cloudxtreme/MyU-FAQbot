3205d7d97818738af8ce8de18f7ee2746cefde5c
Pandu <pandubalraj@gmail.com>
2016-09-21 10:29:29 +0000

    Corrected the dialog.matches()

--- server.js
+++ server.js
@@ -7,27 +7,34 @@ var recognizer = new builder.LuisRecognizer('https://api.projectoxford.ai/luis/v
 var dialog  = new builder.IntentDialog({ recognizers: [recognizer] });
 
 // Get secrets from server environment
-var connector = new builder.ChatConnector({
-  appId: process.env.MICROSOFT_APP_ID,
-  appPassword: process.env.MICROSOFT_APP_PASSWORD
-});
+// var connector = new builder.ChatConnector({
+//   appId: process.env.MICROSOFT_APP_ID,
+//   appPassword: process.env.MICROSOFT_APP_PASSWORD
+// });
 
+// var bot = new builder.UniversalBot(connector);
+
+// // // // Setup Restify Server
+// var server = restify.createServer();
+// // Handle Bot Framework messages
+// server.post('/api/messages', connector.listen());
+// // Serve a static web page
+// server.get(/.*/, restify.serveStatic({
+// 	'directory': '.',
+// 	'default': 'index.html'
+// }));
+
+// server.listen(process.env.port|| process.env.PORT || 3978, function () {
+//     console.log('%s listening to %s', server.name, server.url); 
+// });
+
+// =============================================================================================
+//DEFAULT BOT BUILDER TO CONNECT WITH CONSOLE CONNECTOR
+var builder = require('botbuilder');
+var connector = new builder.ConsoleConnector().listen();
 var bot = new builder.UniversalBot(connector);
-
-// // // Setup Restify Server
-var server = restify.createServer();
-// Handle Bot Framework messages
-server.post('/api/messages', connector.listen());
-// Serve a static web page
-server.get(/.*/, restify.serveStatic({
-	'directory': '.',
-	'default': 'index.html'
-}));
-
-server.listen(process.env.port|| process.env.PORT || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url); 
-});
-
+//DEFAULT BOT BUILDER TO CONNECT WITH CONSOLE CONNECTOR
+// =============================================================================================
 
 // Create bot root dialog
 bot.dialog('/', dialog);
@@ -35,4 +42,20 @@ bot.dialog('/', dialog);
 // =============================================================================================
 // DIALOG MATCH AND STATIC RESPONSE
 
-dialog.matches('Insurance', sourceFile.Insurance);
+dialog.matches('Insurance', builder.DialogAction.send(sourceFile.Insurance));
+dialog.matches('CIMandate', builder.DialogAction.send(sourceFile.CIMandate));
+dialog.matches('CITypes',  builder.DialogAction.send(sourceFile.CITypes));
+dialog.matches('Policy',  builder.DialogAction.send(sourceFile.Policy));
+dialog.matches('LiabilityPolicy',  builder.DialogAction.send(sourceFile.LiabilityPolicy));
+dialog.matches('CICompPackage',  builder.DialogAction.send(sourceFile.CICompPackage));
+dialog.matches('AddOnCover', builder.DialogAction.send(sourceFile.AddOnCover));
+dialog.matches('Exclusion', builder.DialogAction.send(sourceFile.Exclusion));
+dialog.matches('Duration',  builder.DialogAction.send(sourceFile.Duration));
+dialog.matches('InspectVehicle',  builder.DialogAction.send(sourceFile.InspectVehicle));
+dialog.matches('Premium',  builder.DialogAction.send(sourceFile.Premium));
+dialog.matches('CarEval', builder.DialogAction.send(sourceFile.CarEval));
+dialog.matches('IDV',  builder.DialogAction.send(sourceFile.IDV));
+dialog.matches('PlaceOfReg',  builder.DialogAction.send(sourceFile.PlaceOfReg));
+dialog.matches('DiffCompPremium',  builder.DialogAction.send(sourceFile.DiffCompPremium));
+dialog.matches('Discounts',  builder.DialogAction.send(sourceFile.Match));
+dialog.matches('Deductible',  builder.DialogAction.send(sourceFile.Deductible));
--- sourceFile.js
+++ sourceFile.js
@@ -1,3 +1,27 @@
-module.exports = { 
-Insurance: "Insurance is a contract usually between a large financial institution that offers fixed protection to another person, institution or entity against a possible future loss in return of a predetermined premium. Insurance is a Risk management tool that is taken as a precautionary measure against future risks that may or may not happen."
+module.exports = {
+//App 1
+None:"",
+Insurance: "Insurance is a contract usually between a large financial institution that offers fixed protection to another person, institution or entity against a possible future loss in return of a predetermined premium. Insurance is a Risk management tool that is taken as a precautionary measure against future risks that may or may not happen.",
+CIMandate:"",
+CITypes:"",
+Policy:"",
+LiabilityPolicy:"",
+CICompPackage:"",
+AddOnCover:"",
+Exclusion:"",
+Duration:"",
+InspectVehicle:"",
+Premium:"",
+CarEval:"",
+IDV:"",
+PlaceOfReg:"",
+DiffCompPremium:"",
+Discounts:"",
+Deductible:"",
+Claim:"",
+NCB:"",
+//App 2
+Bonus:"",
+
+
 };
